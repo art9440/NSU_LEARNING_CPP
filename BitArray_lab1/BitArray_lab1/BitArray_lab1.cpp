1c966e4a529f6a9e5499fa4ca04d27757a02afc8
art9440 <art9440@gmail.com>
2024-09-25 12:49:34 +0700

    BitArray is started
    Made construcor, distructor, explicit constructor

---
+++ BitArray_lab1/BitArray_lab1/BitArray_lab1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "BitArray_lab1.h"
+
+BitArray::BitArray() : bit_count(0) {}
+
+BitArray::~BitArray() = default;
+
+explicit BitArray::BitArray(int num_bits, unsigned long value = 0) : bit_count(num_bits){
+	bit_array.resize(BYTE_SIZE + bit_count);
+	if (bit_count != 0 && bit_array.empty())
+		bit_array[0] = value;
+}
