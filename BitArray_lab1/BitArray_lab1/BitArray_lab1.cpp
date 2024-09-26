14fc60c878ef2aea0351dcec9f860c6159c42840
art9440 <art9440@gmail.com>
2024-09-26 22:50:41 +0700

    Cmake update
    Troubles with CMake are resolved. Delete resize function and start making new one

--- BitArray_lab1/BitArray_lab1/BitArray_lab1.cpp
+++ BitArray_lab1/BitArray_lab1/BitArray_lab1.cpp
@@ -5,11 +5,11 @@ BitArray::BitArray() : bit_count(0) {}
 
 BitArray::~BitArray() = default;
 
-explicit BitArray::BitArray(int num_bits, unsigned long value = 0) : bit_count(num_bits){
+/*BitArray::BitArray(int num_bits, unsigned long value = 0) : bit_count(num_bits) {
 	bit_array.resize((num_bits + BYTE_SIZE - 1) / BYTE_SIZE, 0);
 	if (bit_count != 0 && bit_array.empty())
 		bit_array[0] = value;
-}
+}*/
 
 
 BitArray::BitArray(const BitArray& b): bit_array(b.bit_array), bit_count(b.bit_count){}
@@ -29,6 +29,15 @@ BitArray& BitArray::operator=(const BitArray& b) {
 	return *this;
 }
 
-void resize(int num_bits, bool value = false) {
+//void BitArray::resize(int num_bits, bool value = false) {
+	
+//}
+
+void BitArray::clear() {
+	bit_array.clear();
+	bit_count = 0;
+}
+
+void BitArray::push_back(bool bit) {
 
 }
